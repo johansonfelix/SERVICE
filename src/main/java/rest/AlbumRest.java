c02fe5aa741663d8fde39aa1e2580ed94ed4f606
johansonfelix <48577361+johansonfelix@users.noreply.github.com>
2021-02-05 10:44:25 -0500

    AlbumRest service code started

---
+++ src/main/java/rest/AlbumRest.java
@@ -0,0 +1,32 @@
+package rest;
+
+import impl.Albums;
+import pojo.Album;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+
+@Path("album")
+public class AlbumRest {
+    private static Albums albums;
+
+
+    //change return type
+    @POST
+    @Consumes("application/xml")
+    public void createBook(Album album) {
+
+        //check if album ISRC already exists
+        //if ISRC exists then return error - album ISRC already exists
+
+        //else add album and return success message
+
+      albums.addAlbum(album);
+
+
+    }
+
+
+}
--- src/main/java/rest/MyResource.java
+++ src/main/java/rest/MyResource.java
@@ -1,4 +1,4 @@
-package AlbumREST;
+package rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
